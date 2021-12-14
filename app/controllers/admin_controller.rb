bea9f30251527dfc824dc45386a050a3e0c0ce0b
Sean Cafe <seanamir30@gmail.com>
2021-12-14 22:17:47 +0800

    Admin can now create users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,6 +6,21 @@ class AdminController < ApplicationController
         @users = User.where(admin: false)
     end
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+            if @user.save
+                redirect_to admin_index_path
+                # redirect_to @category
+                flash[:notice] = "Category was successfully created."
+            else
+                render :new
+            end
+      end
+
     def show
         @user = User.find(params[:id])
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_120242) do
+ActiveRecord::Schema.define(version: 2021_12_14_122725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2021_12_13_120242) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.boolean "is_approved", default: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
