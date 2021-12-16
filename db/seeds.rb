92d66992c8c62bdaf52b890f4a37dd2599f0deaf
Arianne Canlubo <amcanlubo@gmail.com>
2021-12-16 20:52:36 +0800

    seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.new(
+    email: 'admin@test.com',
+    first_name: 'admin',
+    last_name:'admin',
+    password: 'password',
+    password_confirmation: 'password',
+    admin:true
+)
+
+admin.skip_confirmation!
+admin.save
+
+user = User.new(
+    email: 'user@test.com',
+    first_name: 'user',
+    last_name:'user',
+    password: 'password',
+    password_confirmation: 'password',
+    admin:false
+)
+
+user.skip_confirmation!
+user.save
