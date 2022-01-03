a283e87b5d198fd13717aad4eb22aa9b8c528a5c
Arianne Canlubo <amcanlubo@gmail.com>
2022-01-03 21:00:13 +0800

    added tdd templates

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user do   
+    email { "try@email.com" }
+    password { "password" }
   end
 end
--- spec/models/trading_history_spec.rb
+++ spec/models/trading_history_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe TradingHistory, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should belong to stock" do
+    t = TradingHistory.reflect_on_association(:stock)
+    expect(t.macro).to eq(:belongs_to)
+  end
+
+  it "should belong to user" do
+    t = TradingHistory.reflect_on_association(:user)
+    expect(t.macro).to eq(:belongs_to)
+  end
+
 end
