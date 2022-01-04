540c9587e7651902b3f49dbc4e9b5ad4a484e085
Sean Cafe <seanamir30@gmail.com>
2022-01-04 21:43:49 +0800

    heroku

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -12,7 +12,7 @@ class Stock < ApplicationRecord
 
     def self.iex_api
         IEX::Api::Client.new(
-            publishable_token: ENV['PUBLISHABLE_TOKEN']
+            publishable_token: ENV['PUBLISHABLE_TOKEN'],
             secret_token: ENV['SECRET_TOKEN'],
             endpoint: 'https://sandbox.iexapis.com/v1'
         )
