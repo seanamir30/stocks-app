ef77f71bbca75fad2240ab98edcdaf8d316ea599
Sean Cafe <seanamir30@gmail.com>
2022-01-06 20:54:58 +0800

    heroku

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,13 +5,8 @@ FactoryBot.define do
     first_name {'firstname'}
     last_name {'lastname'}
     password { "password" }
-<<<<<<< HEAD
-    first_name { 'firstname'}
-    last_name { 'lastname'}
-=======
     balance { 99999 }
     is_approved { true }
->>>>>>> baf69dab29c4757f727be584b34a491898fb7cb2
   end
   
 end
